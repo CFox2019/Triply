91a9c94c0f6bd7681c2b20235f289a2d90eb7721
CFox2019 <57727915+CFox2019@users.noreply.github.com>
2020-05-04 22:38:19 -0400

    updated homepage styling

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,9 @@ import './js/navigation'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
-import './styles/form.scss'
 import './styles/header.scss'
+import './styles/inspiration.scss'
+import './styles/where_to.scss'
 
 export {
     handleSubmit
